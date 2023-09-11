61c6befbe0e7630cc104c001997d7cc8aa0f8e62
active <66892517+activ33@users.noreply.github.com>
2023-09-11 13:13:55 +0530

    Update config.py

--- MeshRenameBot/config.py
+++ MeshRenameBot/config.py
@@ -4,24 +4,24 @@ try:
     from .tconfig import Config
 except ImportError:
     class Config:
-        DATABASE_URL = [str, ""]
-        API_HASH = [str, "abcdedf......"]
-        API_ID = [int, 1234567]
-        BOT_TOKEN = [str, "bot:token here"]
+        DATABASE_URL = [str, "mongodb+srv://ankit29:ankit29@cluster0.7gdbq.mongodb.net/?retryWrites=true&w=majority"]
+        API_HASH = [str, "ff8451462d91861a13ffd8a6bb72aa8b"]
+        API_ID = [int, 1464463]
+        BOT_TOKEN = [str, "6561330005:AAE2Cw0mXmdc_RJtMEyVkF5akWBuRfDbJR0"]
         COMPLETED_STR = [str, "▰"]
         REMAINING_STR = [str, "▱"]
-        MAX_QUEUE_SIZE = [int, 5]
+        MAX_QUEUE_SIZE = [int, 30]
         SLEEP_SECS = [int, 10]
-        IS_MONGO = [bool, False]
+        IS_MONGO = [bool, True]
 
         # Access Restriction
         IS_PRIVATE = [bool, False]
-        AUTH_USERS = [list,[123456789]]
-        OWNER_ID = [int, 0]
+        AUTH_USERS = [list,[689061386]]
+        OWNER_ID = [int, 689061386]
 
         # Public username url or invite link of private chat
-        FORCEJOIN = [str,""]
-        FORCEJOIN_ID = [int,-100123465978]
+        FORCEJOIN = [str,"animefiles"]
+        FORCEJOIN_ID = [int,-1328254270]
 
         TRACE_CHANNEL = [int, 0]
 
